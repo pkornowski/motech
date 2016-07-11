ed7bbdbda9652b368b63a828bc5a80445da27bef
Przemyslaw Kornowski <pkornowski@soldevelo.com>
2016-07-11 10:42:34 +0200

    ITests

---
+++ modules/tasks/tasks-test-bundle/src/test/java/org/motech/project/testmodule/it/IntegrationTests.java
@@ -0,0 +1,9 @@
+package org.motech.project.testmodule.it;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TasksActionIT.class})
+public class IntegrationTests {
+}
